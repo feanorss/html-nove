24f09c097f18db4d878d6ebe937f67809eefb719
feanorss <madro.milos@gmail.com>
2024-06-11 20:59:06 +0200

    pre dalibora 2

---
+++ static/script.js
@@ -0,0 +1,20 @@
+for (let i = 10; i>1; i--) {
+console.log(i);
+}
+
+let j = 10;
+while (j>1) {
+    console.log(j);
+    j--;
+}
+
+let cena = 80
+console.log(cena*1.2+" eur")
+
+let cena1 = 100
+console.log(cena1/1.2+" eur")
+
+
+const cafeNameElement = document.getElementById('cafe-name');
+
+cafeNameElement.textContent = 'Milošova kaviareň';
